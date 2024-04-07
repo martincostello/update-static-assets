17cb913dadedaac2e238ae359d6ad46ce435415c
martincostello <martin@martincostello.com>
2024-04-07 19:20:48 +0100

    Use esbuild instead of ncc
    Use esbuild instead of ncc as undici and ncc have incompatibilities.

---
+++ dist/CdnProvider.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});exports.CdnProvider=void 0;var d;(function(e){e.cdnjs="cdnjs",e.jsdelivr="jsdelivr"})(d||(exports.CdnProvider=d={}));
+//# sourceMappingURL=CdnProvider.js.map
