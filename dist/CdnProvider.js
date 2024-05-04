e2f744d86c1b2bc566e9335956f25cbc0cd894de
martincostello <martin@martincostello.com>
2024-05-04 13:22:41 +0100

    Bundle into one file
    Fix esbuild usage so that all code is bundled into `main.js` without any additional files in `dist`.

--- dist/CdnProvider.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});exports.CdnProvider=void 0;var d;(function(e){e.cdnjs="cdnjs",e.jsdelivr="jsdelivr"})(d||(exports.CdnProvider=d={}));
-//# sourceMappingURL=CdnProvider.js.map
